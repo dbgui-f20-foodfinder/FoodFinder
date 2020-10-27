b192f888624839918dd91af1389d69e5ae944fb5
Micklerick617 <mlennon@smu.edu>
2020-10-26 22:48:25 -0500

    /products (get)

--- backend/node/server.js
+++ backend/node/server.js
@@ -41,54 +41,25 @@ connection.connect(function (err) {
 
 //GET /
 app.get('/', (req, res) => {
-  res.status(200).send('Go to 0.0.0.0:3000.');
+  res.status(200).send('Go to http://localhost:3001.');
 });
 
+// -------------------------------------------------------------------------------------
+//                                    PRODUCTS TABLE
+// -------------------------------------------------------------------------------------
 
-//POST /reset
-app.post('/reset', (req, res) => {
-  connection.query('drop table if exists test_table', function (err, rows, fields) {
-    if (err)
-      logger.error("Can't drop table");
-  });
-  connection.query('CREATE TABLE `db`.`test_table` (`id` INT NOT NULL AUTO_INCREMENT, `value` VARCHAR(45), PRIMARY KEY (`id`), UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);', function (err, rows, fields) {
-    if (err)
-      logger.error("Problem creating the table test_table");
-  });
-  res.status(200).send('created the table');
-});
-
-//POST /multplynumber
-app.post('/multplynumber', (req, res) => {
-  console.log(req.body.product);
-
-  connection.query('INSERT INTO `db`.`test_table` (`value`) VALUES(\'' + req.body.product + '\')', function (err, rows, fields) {
-    if (err){
-      logger.error("Problem inserting into test table");
-    }
-    else {
-      res.status(200).send(`added ${req.body.product} to the table!`);
-    }
+// /products
+// This Route is used to retrieve the entire products table.
+app.get('/products', function (req, res) {
+  con.query("SELECT * FROM products", function (err, result, fields) {
+    if (err) throw err;
+    res.end(JSON.stringify(result)); // Result in JSON format
   });
 });
 
-//GET /checkdb
-app.get('/values', (req, res) => {
-  connection.query('SELECT value FROM `db`.`test_table`', function (err, rows, fields) {
-    if (err) {
-      logger.error("Error while executing Query");
-      res.status(400).json({
-        "data": [],
-        "error": "MySQL error"
-      })
-    }
-    else{
-      res.status(200).json({
-        "data": rows
-      });
-    }
-  });
-});
+// -------------------------------------------------------------------------------------
+//                                        OTHER
+// -------------------------------------------------------------------------------------
 
 //connecting the express object to listen on a particular port as defined in the config object.
 app.listen(config.port, config.host, (e) => {
