991234a0267e54b3706664fc019905c8ca1ed309
KadeLehman <klehman@smu.edu>
2020-11-26 00:27:22 -0600

    added frontend EC2 url

--- frontend/src/api/FoodsRepository.js
+++ frontend/src/api/FoodsRepository.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export class FoodsRepository {
-    url = 'http://localhost:8001';
+    url = 'https://ec2-3-137-163-61.us-east-2.compute.amazonaws.com';
 
     getFoods() {
         return new Promise((resolve, reject) => {
